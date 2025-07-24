542aeda272a54de7ff68982956a2a83e9dfbf5fb
kimtien1909206 <hkimttien1236@gmail.com>
2025-07-24 21:14:35 +0700

    Add files via upload

---
+++ BTVN_SS1/CNTT6_SESSION1_B8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int number = 12345;
+
+    int firstNumber = number % 10;
+    int secondNumber = (number %100) / 10;
+    int thirdNumber = (number %1000) / 100;
+    int fourNumber = (number %10000) / 1000;
+    int fiveNumber = (number %100000) / 10000;
+
+    return 0;
+}
