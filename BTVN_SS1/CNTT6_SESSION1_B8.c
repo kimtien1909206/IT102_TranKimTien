15cf655e54ac139973c3fa3cc6cc8d688d43f850
kimtien1909206 <hkimttien1236@gmail.com>
2025-07-24 21:21:30 +0700

    Update CNTT6_SESSION1_B8.c

--- BTVN_SS1/CNTT6_SESSION1_B8.c
+++ BTVN_SS1/CNTT6_SESSION1_B8.c
@@ -7,6 +7,7 @@ int main(){
     int thirdNumber = (number %1000) / 100;
     int fourNumber = (number %10000) / 1000;
     int fiveNumber = (number %100000) / 10000;
-
+    int result = firstNumber * 10000 + secondNumber * 1000 + thirdNumber * 100 + fourNumber * 10 + fiveNumber;
+    printf("result: %d\n", result);
     return 0;
 }
